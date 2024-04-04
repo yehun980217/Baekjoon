76fb9babff535c232975a44688ff1f79772c024f
yehun980217 <148829925+yehun980217@users.noreply.github.com>
2024-04-04 19:12:55 +0900

    [Bronze II] Title: 16진수, Time: 208 ms, Memory: 17632 KB -BaekjoonHub

---
+++ 백준/Bronze/1550. 16진수/16진수.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+        System.out.println(Integer.parseInt(input, 16));
+        sc.close();
+    }
+}
